2d3887ce4b31b1c71d46784f8972717f60221390
Ryan Jeffrey <r.jeffrey@live.com.au>
2025-02-15 20:06:27 +1100

    bodyparser

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ dotenv.config();
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 })
-
 
 
 
