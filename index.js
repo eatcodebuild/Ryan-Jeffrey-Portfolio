93e25af49d1d80b8570be17adc094fb864a0011e
Ryan Jeffrey <r.jeffrey@live.com.au>
2025-02-15 20:09:30 +1100

    update

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-})
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
 
 
 
