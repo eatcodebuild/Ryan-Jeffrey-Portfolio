8bc0eaf99dc777e7fabeee5af2640b759147db27
Ryan Jeffrey <r.jeffrey@live.com.au>
2025-02-15 20:03:38 +1100

    addition of backend

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
