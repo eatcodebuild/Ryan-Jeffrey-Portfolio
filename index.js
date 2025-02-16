7d588d433a4cd1f620109e7980d70126c44fca54
Ryan Jeffrey <r.jeffrey@live.com.au>
2025-02-16 21:00:22 +1100

    added pages

--- index.js
+++ index.js
@@ -18,6 +18,22 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/about.html'))
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/contact.html'))
+});
+
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/blog.html'))
+});
+
+app.get('/portfolio', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/portfolio.html'))
+});
+
 
 
 
