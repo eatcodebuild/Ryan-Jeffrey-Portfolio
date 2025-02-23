368fc5ae4b57031db71c4694fdf74eda708c91ff
Ryan Jeffrey <r.jeffrey@live.com.au>
2025-02-24 09:39:09 +1100

    updated contact form logic

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Handles Displaying of HTML Pages ↓
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
@@ -39,9 +41,37 @@ app.get('/testimonials', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/testimonials.html'))
 });
 
+// Handles contact form ↓
 
+app.post('/send-email', async (req, res) => {
+    const { name, email, message } = req.body;
 
+    try {
+        const transporter = nodeMailer.createTransport({
+            service: 'Outlook',
+            auth: {
+                user: process.env.USER_EMAIL,
+                pass: process.env.USER_PASS
+            },
+        });
 
+        const mailOptions = {
+            from: email,
+            to: process.env.USER_EMAIL,
+            subject: `New Contact Form Message From ${name}!`,
+            text: `You've received a new message from: \n \n Name: ${name} \n Email: ${email} \n Message: ${message}`,
+        };
+
+        await transporter.sendMail(mailOptions); 
+        
+        res.json({ success: true, message: "Email sent successfully!"});
+    }
+
+    catch (error) {
+        console.error("Error sending mail:", error);
+        res.status(500).json({ success: false, message: "Email failed to send"})
+    }
+});
 
 
 
