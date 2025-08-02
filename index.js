c8e1f4ecab3da0abd77680ea0ddb6a0a696ac1ac
Ryan Jeffrey <r.jeffrey@live.com.au>
2025-08-02 21:33:30 +1000

    added 'projects.json'

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const path = require("path");
 const port = process.env.PORT || 3000;
-const nodeMailer = require('nodemailer');
+const nodeMailer = require("nodemailer");
+const fs = require("fs");
 
 dotenv.config();
 
@@ -16,63 +17,69 @@ app.use(express.urlencoded({ extended: true }));
 
 // Handles Displaying of HTML Pages ↓
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/about.html'))
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/about.html"));
 });
 
-app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/contact.html'))
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/contact.html"));
 });
 
-app.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/projects.html'))
+app.get("/projects", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/projects.html"));
 });
 
-app.get('/sitemap', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/sitemap.xml'))
+app.get("/sitemap", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/sitemap.xml"));
 });
 
 // Handles contact form ↓
 
-app.post('/send-email', async (req, res) => {
-    const { name, phone, email, message } = req.body;
+app.post("/send-email", async (req, res) => {
+  const { name, phone, email, message } = req.body;
 
-    try {
-        const transporter = nodeMailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false,
-            auth: {
-                user: "contact.ryanjeffrey@gmail.com",
-                pass: "dhqrewcjriabvgsh"
-            }
-        });
+  try {
+    const transporter = nodeMailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: "contact.ryanjeffrey@gmail.com",
+        pass: "dhqrewcjriabvgsh",
+      },
+    });
 
-        const mailOptions = {
-            from: email,
-            to: "r.jeffrey@live.com.au",
-            subject: `New Contact Form Message From ${name}`,
-            text: `You've received a new message from: \n \n Name: ${name} \n Email: ${email} \n Phone: ${phone} \n \n Message: \n ${message}`,
-        };
+    const mailOptions = {
+      from: email,
+      to: "r.jeffrey@live.com.au",
+      subject: `New Contact Form Message From ${name}`,
+      text: `You've received a new message from: \n \n Name: ${name} \n Email: ${email} \n Phone: ${phone} \n \n Message: \n ${message}`,
+    };
 
-        await transporter.sendMail(mailOptions); 
-        
-        res.json({ success: true, message: "Email sent successfully!"});
-    }
+    await transporter.sendMail(mailOptions);
 
-    catch (error) {
-        console.error("Error sending mail:", error);
-        res.status(500).json({ success: false, message: "Email failed to send"})
-    }
+    res.json({ success: true, message: "Email sent successfully!" });
+  } catch (error) {
+    console.error("Error sending mail:", error);
+    res.status(500).json({ success: false, message: "Email failed to send" });
+  }
 });
 
+app.get("/get/projects", (req, res) => {
+  fs.readFile(path.join(__dirname, "projects.json"), "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to read file" });
+    }
+    res.json(JSON.parse(data));
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`);
+  console.log(`Listening on http://localhost:${port}`);
 });
--- public/animations.js
+++ public/animations.js
@@ -1,92 +1,151 @@
+async function loadProjects() {
+  const response = await fetch("https://www.ryancjeffrey.com/get/projects");
+  const projects = await response.json();
+  console.log(projects.length);
+
+  function projectLayout(project, index) {
+    const div = document.createElement("div");
+    div.classList.add("py-5", "bg-space");
+    const directionClass = index % 2 === 0 ? "flex-lg-row" : "flex-lg-row-reverse";
+    div.innerHTML = `
+      <div class="container pt-5">
+          <div class="row align-items-stretch justify-content-center flex-column-reverse ${directionClass} align-items-center scroll-up px-3 px-lg-0">
+              <div class="col-lg-6 col-xl-7 col-12 p-lg-4 text-color">
+                  <h4 class="display-4 mb-4 mt-5 mt-lg-0">${project.title}</h4>
+                  <p class="lead mb-2">
+                      ${project.description_para_1}
+                  </p>
+                  <p class="lead mb-4"> 
+                      ${project.description_para_2}
+                  </p>
+                  <div class="d-flex gap-2">
+                      <a class="text-decoration-none" href="${project.project_link}" target="_blank" rel="noopener noreferrer">
+                          <button type="button" class="btn btn-primary rounded-3 px-4 shadow-sm mt-4 moveSVG">
+                              <div class="d-flex gap-1 justify-content-center align-items-center">
+                                  <span>Visit website</span>
+                                  <img src="/Images/right-chevron-svgrepo-com.svg" alt="chevron" height="20px" width="auto">
+                              </div>
+                          </button>
+                      </a>
+                      ${
+                        project.has_repo
+                          ? `
+                        <a class="text-decoration-none" href="${project.github_link}" target="_blank" rel="noopener noreferrer">
+                          <button type="button" class="btn btn-outline-primary rounded-3 px-4 shadow-sm mt-4 moveSVG">
+                              <div class="d-flex gap-1 justify-content-center align-items-center">
+                                  <span>View code</span>
+                                  <img src="/Images/right-chevron-svgrepo-com.svg" alt="chevron" height="20px" width="auto">
+                              </div>
+                          </button>
+                      </a>
+                        `
+                          : ""
+                      }
+                  </div>
+              </div>
+              <div class="col-lg-6 col-xl-5 col-12 mx-auto d-flex align-items-center justify-content-center">
+                  <img src="${project.img}" class="img-fluid rounded space-shadow" alt="Project Thumbnail">
+              </div>
+          </div>
+      </div>
+    `;
+    return div;
+  }
+
+  const list = document.getElementById("projects-list");
+  list.innerHTML = "";
+
+  projects.forEach((project, index) => {
+    const formattedProject = projectLayout(project, index);
+    list.appendChild(formattedProject);
+  });
+}
+
 // Scroll to appear function ↓
 
-document.addEventListener("DOMContentLoaded", function () {
-    const scrollItems = document.querySelectorAll(".scroll-up, .scroll-left, .scroll-right");
+function checkScroll() {
+  const scrollItems = document.querySelectorAll(".scroll-up, .scroll-left, .scroll-right");
+  const triggerBottom = window.innerHeight * 0.85;
 
-    function checkScroll() {
-        const triggerBottom = window.innerHeight * 0.85;
+  scrollItems.forEach((item) => {
+    const itemTop = item.getBoundingClientRect().top;
 
-        scrollItems.forEach((item) => {
-            const itemTop = item.getBoundingClientRect().top;
-
-            if (itemTop < triggerBottom) {
-                item.classList.add("active");
-            }
-        });
+    if (itemTop < triggerBottom) {
+      item.classList.add("active");
     }
+  });
+}
 
-    window.addEventListener("scroll", checkScroll);
-    checkScroll();
+document.addEventListener("DOMContentLoaded", async () => {
+  await loadProjects();
+  checkScroll();
+  window.addEventListener("scroll", checkScroll);
 });
 
-
 function darkenNav() {
-    const nav = document.getElementsByTagName("nav")[0];
-    if (window.scrollY > 100) {
-        nav.classList.remove("nav-transparent");
-        nav.classList.add("darken-nav");
-    } else {
-        nav.classList.remove("darken-nav");
-        nav.classList.add("nav-transparent");
-    }
+  const nav = document.getElementsByTagName("nav")[0];
+  if (window.scrollY > 100) {
+    nav.classList.remove("nav-transparent");
+    nav.classList.add("darken-nav");
+  } else {
+    nav.classList.remove("darken-nav");
+    nav.classList.add("nav-transparent");
+  }
 }
 window.addEventListener("scroll", darkenNav);
 window.addEventListener("load", darkenNav);
 
-
 function darkenNavMenuDropdown() {
-    const nav = document.getElementsByTagName("nav")[0];
-    if (!nav.classList.contains("darken-nav")) {
-        nav.classList.remove("nav-transparent");
-        nav.classList.add("darken-nav");
-    } 
+  const nav = document.getElementsByTagName("nav")[0];
+  if (!nav.classList.contains("darken-nav")) {
+    nav.classList.remove("nav-transparent");
+    nav.classList.add("darken-nav");
+  }
 }
 
 const hamburgerIcons = document.querySelectorAll(".navbar-toggler");
-hamburgerIcons.forEach(icon => {
-    icon.addEventListener("click", () => {
-        darkenNavMenuDropdown();
-    });
+hamburgerIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    darkenNavMenuDropdown();
+  });
 });
-
-
 
 // Stars
 particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 200,
-        "density": { "enable": true, "value_area": 800 }
-      },
-      "color": { "value": "#ffffff" },
-      "shape": {
-        "type": "circle",
-        "stroke": { "width": 0, "color": "#000000" }
-      },
-      "opacity": {
-        "value": 1,
-        "random": true
-      },
-      "size": {
-        "value": 2,
-        "random": true
-      },
-      "line_linked": {
-        "enable": false
-        },
-      "move": {
-        "enable": true,
-        "speed": 1,
-        "direction": "none",
-        "out_mode": "bounce"
-      }
+  particles: {
+    number: {
+      value: 200,
+      density: { enable: true, value_area: 800 },
     },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": { "enable": false },
-        "onclick": { "enable": false }
-      }
+    color: { value: "#ffffff" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
     },
-    "retina_detect": true
+    opacity: {
+      value: 1,
+      random: true,
+    },
+    size: {
+      value: 2,
+      random: true,
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: "none",
+      out_mode: "bounce",
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: false },
+      onclick: { enable: false },
+    },
+  },
+  retina_detect: true,
 });
